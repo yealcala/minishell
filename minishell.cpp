8698dee2fed47326475e92ef4c5a2adb3d84f324
Yboy06 <yboy06yt@gmail.com>
2025-11-29 13:47:17 +0100

    Added minishell with all extras.

---
+++ minishell.cpp
@@ -0,0 +1,80 @@
+/** 
+ * minishell_2.cpp
+ * @copyright esquema-shell.cpp
+ * @copyright Eloy J. Mata & Francisco J. García Izquierdo
+ * @author: Yeray Alcala Paz
+ * 
+ * CÓMO EJECUTAR EL PROGRAMA :
+ *  - Versión interactiva: Ejecutar ./a.out
+ *  - Versión fichero de comandos: Ejecutar ./a.out fichero
+ */
+
+#include <iostream>   // Necesario para cout
+#include <fstream>    // Necesario para ifstream
+#include <cstdlib>    // Necesario para la función exit
+
+#include <unistd.h> // Necesario para chdir
+#include <sys/wait.h> // Neceasario para la función wait
+
+#include "utils/ioutils.h" // Utilidades para io.
+                            // Necesario para la función read
+#include "utils/strutils.h"  // Utilidades para string.
+                            // Necesario para la función strsplit
+#include "utils/cmdutils.h"  // Utilidades para string.
+                            // Necesario para la función ejecutar
+using namespace std;
+
+void ejecutar(char cmd[]);
+
+const int INPUT_MAX_SIZE = 255;
+static char EXIT[] = "salir";
+static char CD[] = "cd";
+
+int main(int argc, char* argv[]) {
+    char cwd[INPUT_MAX_SIZE+1];
+    char cmd[INPUT_MAX_SIZE+1];
+    if (argc == 1) { // Interactivo.
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+            cout << cwd << ": ";
+        else cout << "/route/error" << ": ";
+        read(cmd, INPUT_MAX_SIZE);
+        
+        char **trozos;
+
+        while(strcmp(cmd, EXIT) != 0) {
+            int num = strsplit(cmd, ";", trozos);
+            for(int i = 0; i < num; i++)
+                ejecutar(*(trozos+i));
+            
+            if (getcwd(cwd, sizeof(cwd)) != NULL)
+                cout << cwd << ": ";
+            else cout << "/route/error" << ": ";
+
+            read(cmd, INPUT_MAX_SIZE);
+        }
+        free(trozos);
+    } else if (argc == 2) {
+        ifstream entrada;
+        entrada.open(argv[1]);
+        if (entrada.fail()) { // Si no existe el archivo introducido...
+            cerr << "No puede abrirse el archivo " << argv[1] << endl;
+            exit(2);
+        } else {
+            char **trozos;
+            entrada.getline(cmd, INPUT_MAX_SIZE, '\n'); 
+            while (! entrada.eof() && strcmp(cmd, EXIT) != 0 ) {
+                int num = strsplit(cmd, ";", trozos);
+                for(int i = 0; i < num; i++)
+                    ejecutar(strtrim(*(trozos+i)));
+                entrada.getline(cmd, INPUT_MAX_SIZE, '\n'); 
+            }
+            free(trozos);
+            entrada.close();
+        }
+    } else { // Si se introducen 2 o más argumentos...
+        cerr << "Número de argumentos incorrecto" << endl;
+        exit(1);
+    }
+    
+    exit(0);
+}
--- utils/cmdutils.cpp
+++ utils/cmdutils.cpp
@@ -6,16 +6,17 @@
 #include <cstdlib>    // Necesario para exit                      
 #include <cstring>    // Necesario para la función strcpy
 #include <sys/wait.h> // Necesario para la función wait
-
+#include <fcntl.h>  // Necesario para llamadas sistema archivos: open, creat...
+#include <pwd.h> // Necesario para obtener el Home del usuario.
 using namespace std;
 
 static char CD[] = "cd";
 
-void getredir(char* str, char* & in, char* & out) {
+void getredir(char* str, char* & in, char* & out, char* & err, char* & appout) {
     int i = 0;
-    in = NULL; out = NULL;
+    in = NULL; out = NULL; err = NULL; appout = NULL;
     const int n = strlen(str);
-    for(int i = 0; i < n; i++)
+    for(int i = 0; i < n; i++) {
         switch(*(str+i)) {
             case '<': {
                 *(str+i) = '\0';
@@ -24,19 +25,40 @@ void getredir(char* str, char* & in, char* & out) {
             }
             case '>': {
                 *(str+i) = '\0';
-                out = str+i+1;
+                if(i > 0 && *(str+i-1) == '2') {
+                    *(str+i-1) = '\0';
+                    err = str+i+1;
+                } else if(i < n-1 && *(str+i+1) == '>') {
+                    *(str+i+1) = '\0';
+                    appout = str+i+2;
+                    out = NULL;
+                } else {
+                    out = str+i+1;
+                    appout = NULL;
+                }
+
                 break;
             }
         }
+    }
+
     in = strtrim(in);
     if (in && !in[0]) in = NULL;
     out = strtrim(out);
     if (out && !out[0]) out = NULL;
+    err = strtrim(err);
+    if (err && !err[0]) err = NULL;
+    appout = strtrim(appout);
+    if (appout && !appout[0]) appout = NULL;
 }
 
-void ejecutar(char cmd[]) {
+void ejecutar(char command[]) {
+    char** cmdSplitComments;
+    strsplit(command, "#", cmdSplitComments);
+    char *pathin, *pathout, *patherr, *pathappout;
+    getredir(cmdSplitComments[0], pathin, pathout, patherr, pathappout); // Considerar y eliminar redirecciones.
     char** trozos_cmd; 
-    int count = strsplit(cmd, " \t", trozos_cmd);
+    int count = strsplit(cmdSplitComments[0], " \t", trozos_cmd); // Considerar y separar argumentos.
 
     if(strcmp(trozos_cmd[0], CD) == 0) {
         // Aquí no usamos fork porque si no sólo se
@@ -44,27 +66,61 @@ void ejecutar(char cmd[]) {
         if(count > 2)
             cerr << "cd: Too many arguments" << endl;
         else {
-            if(chdir(trozos_cmd[1]) == 0) {
-                cout << "cd: Moved to " << trozos_cmd[1] << endl;
+            if(count == 1 || (count == 2 && trozos_cmd[1][0] == '~')){
+                // Miralos la varable $HOME. Como podría ser NULL, por si acaso miramos también
+                // su directorio HOME en su fila del passwd, aunque esté desaconsejado (es como un fallback).
+                if(chdir(getenv("HOME") != NULL ? getenv("HOME") : getpwuid(getuid())->pw_dir) != 0) {
+                    perror("cd: Cannot move to home");
+                }
             } else {
-                perror("cd: Cannot move to directory");
+                if(chdir(trozos_cmd[1]) != 0) {
+                    perror("cd: Cannot move to directory");
+                }
             }
+            
         }
     } else {
         pid_t pid = fork();
-        if (pid == -1)
-        perror("Error al crear un proceso\n");
-
+        if (pid == -1) perror("Error whilst creating child process.\n");
         else if (pid == 0) {  // Proceso hijo
-        execvp(trozos_cmd[0], trozos_cmd);
-        perror("Comando desconocido");
-        free(trozos_cmd);
-        exit(-1);
+            if(pathin != nullptr) {
+                close(0);
+                int fd = open(pathin, O_RDONLY);
+                if ( fd == -1 ){
+                    cerr << "I can't open file " << pathin << ". Is it readable?" << endl;
+                    exit(-1);
+                }
+            }
+            if(pathout != nullptr) {
+                close(1);
+                int fd = open(pathout, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+                if ( fd == -1 ){
+                    cerr << "I can't open file " << pathin << ". Is it writeable?" << endl;
+                    exit(-1);
+                }
+            } else if(pathappout != nullptr) {
+                close(1);
+                int fd = open(pathappout, O_WRONLY|O_CREAT|O_APPEND, 0644);
+                if ( fd == -1 ){
+                    cerr << "I can't open file " << pathappout << ". Is it writeable?" << endl;
+                    exit(-1);
+                }
+            }
+            if(patherr != nullptr) {
+                close(2);
+                int fd = open(patherr, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+                if ( fd == -1 ){
+                    cerr << "I can't open file " << patherr << ". Is it writeable?" << endl;
+                    exit(-1);
+                }
+            }
+            execvp(trozos_cmd[0], trozos_cmd);
+            perror("Unknown command!\n");
+            free(trozos_cmd);
+            exit(-1);
         }
     }
 
-    
-    
     wait(NULL);
     free(trozos_cmd); 
 }
